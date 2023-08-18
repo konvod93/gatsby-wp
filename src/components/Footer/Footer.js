25d57a7ac1abf490c90609e2a2fa79120efe7c58
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-08-18 12:25:39 +0300

    created Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterWrapper } from "./Footer.styles";
+
+
+const Footer = () => (
+    <FooterWrapper>
+        <p>
+            Do you want to get updates and news from Cake It? Contakt us on the adress below.
+        </p>
+        <p>Cake It | Cake Boulevard 10 | Fiction Land | cakeit@cakeit.fiction</p>
+    </FooterWrapper>
+)
+
+export default Footer
