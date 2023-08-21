396d8349c424b48c4e7ac03501d98f7ab7c3b610
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-08-21 16:47:07 +0300

    created CTAArea component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import SEO from "../components/SEO/SEO"
 import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
+import CTAArea from "../components/CTAArea/CTAArea"
 
 
 
@@ -9,6 +10,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <CTAArea />
   </Layout>
 )
 
